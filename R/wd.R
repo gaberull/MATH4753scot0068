9332ba8b2a8c410c6c1b5fb48666dbd842e4e047
Austin Gabe Scott <austin.g.scott-1@ou.edu>
2020-02-07 21:05:01 -0600

    Added function from lab 1

---
+++ R/wd.R
@@ -0,0 +1,15 @@
+#' Shows the current working directory. From Lab 1.
+#'
+#' Takes no input
+#'
+#' @param
+#'
+#' @return Working Directory
+#' @export
+#'
+#' @examples
+#'  wd()
+wd=function()
+{
+  getwd()
+}
