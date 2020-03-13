94c69514ae3a35ce61de8b2c632865638222d6fb
Austin Gabe Scott <austin.g.scott-1@ou.edu>
2020-03-13 18:35:47 -0500

    Commiting mycltb function from lab8 and fire.R from project 1.

---
+++ R/fire.R
@@ -0,0 +1,13 @@
+#' Fire Damage Data Set
+#'
+#' A dataset with 2 variables both of which are quantitative.
+#' Interest is in the amount of Damage in dollars and distance from the fire station in miles
+#'
+#' @format A data frame with 15 rows and 2 variables:
+#' \describe{
+#'   \item{DISTANCE}{Distance from fire station in miles}
+#'   \item{DAMAGE}{x 1000 dollars in damage}
+#'   ...
+#' }
+#' @source \url{https://www.crcpress.com/Statistics-for-Engineering-and-the-Sciences-Sixth-Edition/Mendenhall-Sincich/p/book/9781498728850}
+"fire"
